fda4acb5f77ac7efad13b7d988dacc05a0d88057
eliottsegard <eliott203@gmail.com>
2017-03-25 20:52:52 +0100

    Correction of an error that occurred in the previous conflict management

--- working copy/Player.h
+++ working copy/Player.h
@@ -1,6 +1,6 @@
 #ifndef PLAYER_H_INCLUDED
 #define PLAYER_H_INCLUDED
-#include "Boat.h"
+#include "Constantes.h"
 
 ///Player class
 class Player
@@ -8,12 +8,12 @@ class Player
     private:
         int m_player_number;
         bool m_isLost;
+        //Table that keeps in memory each player's shots fired - public because is not important
+        char m_shots_fired[NBLIGNES][NBCOLS];
 
     public:
         //Default constructor
-         std::vector<Boat> m_boats;
-
-         Player();
+        Player();
         //Overcharged constructor
         Player(int player_number, bool isLost);
         //Destructor
@@ -22,11 +22,12 @@ class Player
         //Getters
         int getPlayer_number()const;
         bool getIsLost()const;
+        char getShotsFired(int i, int j)const;
 
         //Setters
-
         void setPlayerNumber(int player_number);
         void setIsLost(bool isLost);
+        void setShotsFired(int i, int j, char a);
 
         ///Methods
 
