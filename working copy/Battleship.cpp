ecc809096c9892f80c5bb879554358cb08de316f
eliottsegard <eliott203@gmail.com>
2017-03-25 12:47:23 +0100

    Ajou des methodes isOnThatSpot et isHit

--- working copy/Battleship.cpp
+++ working copy/Battleship.cpp
@@ -3,11 +3,102 @@
 //Constructor
 Battleship::Battleship(): Boat()
 {
-
+    for(int i=0; i<BATTLESHIP_LENGTH; i++)
+    {
+        bool b= false;
+        m_hit_map.push_back(b);
+    }
 }
 
-//Is the boat currently on the specified case?
+//Is the boat hit on the specified case?
 bool Battleship::isHit(int pos_i, int pos_j)
+{
+    switch(m_dir)
+    {
+        case UP:
+            for(int i=0; i<m_length; i++)
+            {
+                //Only j varies
+                if(pos_i== m_pos_i && pos_j== m_pos_j-i)
+                {
+                    //If the boat is not already hit on that spot
+                    if(!m_hit_map[i])
+                    {
+                        //Set the spot as hit on the hit map
+                        m_hit_map[i]= true;
+                        //The boat is hit: return true
+                        return true;
+                    }
+
+                }
+            }
+            return false; //If the boat wasn't hit return false
+            break;
+
+        case DOWN:
+            for(int i=0; i<m_length; i++)
+            {
+                //Only j varies
+                if(pos_i== m_pos_i && pos_j== m_pos_j+i)
+                {
+                    //If the boat is not already hit on that spot
+                    if(!m_hit_map[i])
+                    {
+                        //Set the spot as hit on the hit map
+                        m_hit_map[i]= true;
+                        //The boat is hit: return true
+                        return true;
+                    }
+                }
+            }
+            return false; //If the boat wasn't hit return false
+            break;
+
+        case RIGHT:
+            for(int i=0; i<m_length; i++)
+            {
+                //Only i varies
+                if(pos_j== m_pos_j && pos_i== m_pos_i+i)
+                {
+                    //If the boat is not already hit on that spot
+                    if(!m_hit_map[i])
+                    {
+                        //Set the spot as hit on the hit map
+                        m_hit_map[i]= true;
+                        //The boat is hit: return true
+                        return true;
+                    }
+                }
+            }
+            return false; //If the boat wasn't hit return false
+            break;
+
+        case LEFT:
+            for(int i=0; i<m_length; i++)
+            {
+                //Only i varies
+                if(pos_j== m_pos_j && pos_i== m_pos_i-i)
+                {
+                    //If the boat is not already hit on that spot
+                    if(!m_hit_map[i])
+                    {
+                        //Set the spot as hit on the hit map
+                        m_hit_map[i]= true;
+                        //The boat is hit: return true
+                        return true;
+                    }
+                }
+            }
+            return false; //If the boat wasn't hit return false
+            break;
+
+        default: return false;
+
+    }
+}
+
+//Is the boat on that specified spot? If yes return true (used for conflict testing)
+bool Battleship::isOnThatSpot(int pos_i, int pos_j)
 {
     switch(m_dir)
     {
@@ -21,7 +112,7 @@ bool Battleship::isHit(int pos_i, int pos_j)
                     return true;
                 }
             }
-            return false;
+            return false; //If the boat wasn't hit return false
             break;
 
         case DOWN:
@@ -34,7 +125,7 @@ bool Battleship::isHit(int pos_i, int pos_j)
                     return true;
                 }
             }
-            return false;
+            return false; //If the boat wasn't hit return false
             break;
 
         case RIGHT:
@@ -47,7 +138,7 @@ bool Battleship::isHit(int pos_i, int pos_j)
                     return true;
                 }
             }
-            return false;
+            return false; //If the boat wasn't hit return false
             break;
 
         case LEFT:
@@ -60,7 +151,7 @@ bool Battleship::isHit(int pos_i, int pos_j)
                     return true;
                 }
             }
-            return false;
+            return false; //If the boat wasn't hit return false
             break;
 
         default: return false;
